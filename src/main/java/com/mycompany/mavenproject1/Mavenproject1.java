a9fbd45a824ec3bf4aebcf2a379a8de1efa91706
ZunaraMughal <117177002+ZunaraMughal@users.noreply.github.com>
2023-01-16 12:20:47 -0500

    Update Mavenproject1.java

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,7 +11,7 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! V2 cloning");
     }
     public void test(){
         
